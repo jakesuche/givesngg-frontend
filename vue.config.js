bf8c1e7eade8a9c7058a23a3b8615bde6e5d5714
Chidiebere Uchechukwu <uchechidi9@gmail.com>
2021-02-18 18:56:16 +0100

    heroku deploy

--- vue.config.js
+++ vue.config.js
@@ -1,11 +1,14 @@
 
 module.exports = {
-    devServer: {
+    c: {
       proxy: {
         "^/api": {
           target: "https://givesng-staging.herokuapp.com",
-          ws:true,
-          secure:false
+          // ws:true,
+          // secure:false
+          pathRewrite: { "^/api/": "/api/" },
+        changeOrigin: true,
+        
         },
       },
     },
